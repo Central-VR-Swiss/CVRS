fac684d518de29ba26580de9bb8fbda87552f97e
16D8101012J <33050292+16D8101012J@users.noreply.github.com>
2017-10-31 15:29:12 +0900

    Update MapPoints.java
    Add line89-106 to make function that enable us to print the data as VRML(.wrl).

--- src/centralvrswiss/MapPoints.java
+++ src/centralvrswiss/MapPoints.java
@@ -85,3 +85,23 @@ public class MapPoints {
         }
     }
 }
+
+import java.io.*;
+public class JAVA_to_VRML 
+{
+	public static void main(String[] args)
+	{
+		try{
+			PrintWriter pw = new PrintWriter
+			(new BufferedWriter(new FileWriter("Test.wrl")));
+			
+			
+			
+			pw.close();
+		}
+		catch(IOException e){
+			System.out.println("入出力エラー");
+		}
+	}
+}
+
